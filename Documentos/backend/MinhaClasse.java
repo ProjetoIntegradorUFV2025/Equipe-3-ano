4709a4d4533c9031813e93d3bc4848fff12f49bb
amandacmelo <amanda.c.assuncao@ufv.br>
2025-08-30 18:56:57 -0300

    feat: implementa classe Minha Classe

---
+++ Documentos/backend/MinhaClasse.java
@@ -0,0 +1,11 @@
+public class MinhaClasse {
+    private String mensagem;
+
+    public MinhaClasse(String mensagem) {
+        this.mensagem = mensagem;
+    }
+
+    public void exibirMensagem() {
+        System.out.println(mensagem);
+    }
+}
