f7b38c34fa0540189e506c1be64566b0f52c00f9
Eufrausino <gustavo.medeiros@ufv.br>
2025-09-06 14:50:36 -0300

    Feat: Adicionando versão limpa do ambiente de desenvolvimento

---
+++ desconecta/backend/Desconecta/src/test/java/ufv/desconecta/Desconecta/DesconectaApplicationTests.java
@@ -0,0 +1,13 @@
+package ufv.desconecta.Desconecta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesconectaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
