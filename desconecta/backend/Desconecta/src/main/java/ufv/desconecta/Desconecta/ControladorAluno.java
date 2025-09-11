c66bbdc5230d7ca1f2f8f7acc8dadf0c4319fed8
GuilhermePianetti <guipianetti@hotmail.com>
2025-09-11 20:53:11 -0300

    Feat: implementação correta classes AcessoBDAluno e ControladorAluno

--- desconecta/backend/Desconecta/src/main/java/ufv/desconecta/Desconecta/ControladorAluno.java
+++ desconecta/backend/Desconecta/src/main/java/ufv/desconecta/Desconecta/ControladorAluno.java
@@ -8,14 +8,14 @@ import lombok.NoArgsConstructor;
 public class ControladorAluno {
     private AcessoBDAluno acessoBDAluno;
 
-    private Boolean validarDadosAluno(String apelido, String senha){
+    public Boolean validarDadosAluno(String apelido, String senha){
         if(apelido == null || apelido.isEmpty() || senha == null || senha.isEmpty()){
             return false;
         }
         return true;
     }
 
-    private Boolean realizarCadastro(String apelido, String senha){
+    public Boolean realizarCadastro(String apelido, String senha){
         if(validarDadosAluno(apelido, senha)){
             if(!acessoBDAluno.verficarApelidoExistente(apelido)){
                 Aluno aluno = new Aluno(apelido, senha);
@@ -30,7 +30,7 @@ public class ControladorAluno {
         
     }
 
-    Boolean autenticarAluno(String apelido, String senha){
+    public Boolean autenticarAluno(String apelido, String senha){
         if(validarDadosAluno(apelido, senha)){
             Aluno aluno = acessoBDAluno.buscarApelido(apelido);
             if(aluno != null && aluno.getSenha().equals(senha)){
