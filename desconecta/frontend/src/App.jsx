3536c3fbb81cb83cb5cbd37efd51d23668621efd
Gabriel_Duarte <88411632+gabriel2036@users.noreply.github.com>
2025-09-12 15:56:02 -0300

    Feat: Tela de login
    Adicionando um esqueleto inicial da tela de login

--- desconecta/frontend/src/App.jsx
+++ desconecta/frontend/src/App.jsx
@@ -1,36 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
+import TelaInicial from "./components/TelaInicial";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [tela, setTela] = useState("inicial");
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {tela === "inicial" && (
+        <TelaInicial
+          irParaLogin={() => setTela("login")}
+          irParaCadastro={() => setTela("cadastro")}
+        />
+      )}
+      {tela === "login" && <h1 className="text-3xl">Tela de Login</h1>}
+      {tela === "cadastro" && <h1 className="text-3xl">Tela de Cadastro</h1>}
     </>
-  )
+  );
 }
 
-export default App
-
+export default App;
