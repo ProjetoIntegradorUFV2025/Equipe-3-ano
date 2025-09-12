0ae4322338f37ddcce8ba0ff7adc7e33562afc85
GuilhermePianetti <guipianetti@hotmail.com>
2025-09-11 22:49:54 -0300

    Feat:  mais algumas alterações

--- desconecta/backend/Desconecta/src/main/java/ufv/desconecta/Desconecta/AcessoBDAluno.java
+++ desconecta/backend/Desconecta/src/main/java/ufv/desconecta/Desconecta/AcessoBDAluno.java
@@ -1,27 +1,57 @@
 package ufv.desconecta.Desconecta;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 
-
+@Entity
+@Table(name = "TB_Aluno")
+@NoArgsConstructor
+@AllArgsConstructor
 public class AcessoBDAluno {
 
+    @Id
+    @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
+    private Long PK_Aluno;
 
-    private Boolean inserirAluno(Aluno aluno){
+    @Column(nullable = false)
+    private String apelido;
+
+    @Column(nullable = false)   
+    private String senha;
+
+    @Column(nullable = false)
+    private Long id;
+
+    @Column(nullable = false)
+    private String progresso;
+
+
+
+
+
+    public Boolean inserirAluno(Aluno aluno){
         return true;
     }
 
-    private Aluno buscarApelido(String apelido){
-        return null;
+    public Aluno buscarApelido(String apelido){
+
+        
     }
 
-    private Boolean verificarConexao(){
+    public Boolean verificarConexao(){
         return true;
     }
 
-    private Boolean verficarApelidoExistente(String apelido){
+    public Boolean verficarApelidoExistente(String apelido){
         return false;
     }
 
-    private Boolean apagarAluno(String apelido){
+    public Boolean apagarAluno(String apelido){
         return true;
     }
 
