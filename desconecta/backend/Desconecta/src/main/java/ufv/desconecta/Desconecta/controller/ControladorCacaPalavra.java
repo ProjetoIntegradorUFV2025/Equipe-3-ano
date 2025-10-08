eff447106ca3cf2bcd9ffedb4da67e35187ee981
Gabriel_Duarte <88411632+gabriel2036@users.noreply.github.com>
2025-10-08 00:23:55 -0300

    Feat: Back CSU07

---
+++ desconecta/backend/Desconecta/src/main/java/ufv/desconecta/Desconecta/controller/ControladorCacaPalavra.java
@@ -0,0 +1,39 @@
+package ufv.desconecta.Desconecta.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import ufv.desconecta.Desconecta.repository.AcessoBDCacaPalavras;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/cacaPalavras")
+@CrossOrigin("*")
+public class ControladorCacaPalavra {
+
+    @Autowired
+    private AcessoBDCacaPalavras acessoBDCacaPalavras;
+
+    @GetMapping("/verificarAgrupamento/{id}")
+    public String verificarAgrupamento(@PathVariable("id") int Pk_CacaPalavras,
+                                       @RequestParam String tentativa){
+        List<String> respostas = acessoBDCacaPalavras.getRespostas(Pk_CacaPalavras);
+        System.out.println(tentativa);
+        System.out.println(respostas);
+        if(respostas.isEmpty()){
+            return "Erro no acesso ao banco de dados.";
+        } else if (respostas.contains(tentativa)) {
+            return  "Resposta está correta.";
+        }
+        return "Resposta está incorreta.";
+    }
+
+    @GetMapping("/getList/{id}")
+    public List<String> getListaRespostas(@PathVariable("id") int PK_CacaPalavras){
+        List<String> respostas = acessoBDCacaPalavras.getRespostas(PK_CacaPalavras);
+        System.out.println(respostas);
+        return respostas;
+    }
+
+}
