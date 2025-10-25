5d0ea8cbe98df63d5dcbc09147072c9d3faea0fb
DaniiMelo <contato.danielmoura@gmail.com>
2025-10-25 11:55:39 -0300

    Feat: commit classe Desafio.java

--- desconecta/backend/Desconecta/src/main/java/ufv/desconecta/Desconecta/Desafio.java
+++ desconecta/backend/Desconecta/src/main/java/ufv/desconecta/Desconecta/Desafio.java
@@ -1,5 +1,45 @@
 package ufv.desconecta.Desconecta;
 
 public class Desafio {
-    
+    private int id;
+    private EnumTiposDesafios tipoDesafio;
+    private int pontuacaoDesafio;
+    private boolean isConcluido;
+
+        private Desafio(int id, EnumTiposDesafios tipoDesafio, int pontuacaoDesafio, boolean isConcluido){
+            this.id = id;
+            this.tipoDesafio = tipoDesafio;
+            this.pontuacaoDesafio = pontuacaoDesafio;
+            this.isConcluido = isConcluido;
+}
+
+        
+    public int getId() {
+        return id;
+    }
+
+    public EnumTiposDesafios getTipoDesafio() {
+        return tipoDesafio;
+    }
+
+    public int getPontuacaoDesafio() {
+        return pontuacaoDesafio;
+    }
+
+    public boolean isConcluido() {
+        return isConcluido;
+    }
+
+
+    public void setTipoDesafio(EnumTiposDesafios tipoDesafio) {
+        this.tipoDesafio = tipoDesafio;
+    }
+
+    public void setPontuacaoDesafio(int pontuacaoDesafio) {
+        this.pontuacaoDesafio = pontuacaoDesafio;
+    }
+
+    public void setConcluido(boolean concluido) {
+        isConcluido = concluido;
+    }
 }
