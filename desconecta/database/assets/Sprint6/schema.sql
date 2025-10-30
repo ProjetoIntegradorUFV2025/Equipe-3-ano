d8e9a72bfe6b3af7c92fbd095c51631af391411f
Francyelle Serrano <francyelle.serrano@ufv.br>
2025-10-30 12:00:09 -0300

    feat: adiciona sql das consultas realizadas

--- desconecta/database/assets/Sprint6/schema.sql
+++ desconecta/database/assets/Sprint6/schema.sql
@@ -1,1 +1,24 @@
 CREATE SCHEMA desconecta;
+
+-- 1) Consultar quantos alunos estão cadastrados no sistema.
+USE desconecta;
+
+SELECT COUNT(*) AS total_de_alunos
+FROM tb_aluno;
+
+--2) Escolher 1 desses alunos, e verificar quais ilhas estão desbloqueadas
+USE desconecta;
+
+SELECT 
+    aluno.apelido,
+    ilha.nome_ilha,
+    ilha.estado,
+    ilha.foi_jogada
+FROM 
+    tb_aluno AS aluno
+JOIN 
+    tb_progresso_aluno AS progresso ON aluno.pk_aluno = progresso.fk_aluno
+JOIN 
+    tb_ilha AS ilha ON progresso.pk_progresso_aluno = ilha.pk_progresso_aluno
+WHERE 
+    aluno.apelido = 'Pedrinho';
